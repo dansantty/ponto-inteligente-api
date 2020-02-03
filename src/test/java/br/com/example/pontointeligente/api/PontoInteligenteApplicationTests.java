ae6ba31701c9a324f9d218eb5238bfacd5abc965
Danilo Barbosa <dansantty@gmail.com>
2020-02-02 23:07:38 -0300

    Update

---
+++ src/test/java/br/com/example/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
