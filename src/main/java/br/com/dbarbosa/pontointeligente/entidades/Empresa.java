1f0ad42129c91533ab42e95f5220c149e197219b
Danilo Barbosa <dansantty@gmail.com>
2020-02-04 13:19:11 -0300

    criacao de entidades

--- src/main/java/br/com/dbarbosa/pontointeligente/entidades/Empresa.java
+++ src/main/java/br/com/dbarbosa/pontointeligente/entidades/Empresa.java
@@ -1,7 +1,92 @@
 package br.com.dbarbosa.pontointeligente.entidades;
 
-public class Empresa {
-	
-	
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = -630563392061642296L;
+
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+
+	public Empresa() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "razao_social", nullable = false)
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@Column(name = "cnpj", nullable = false)
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Column(name = "data_criacao", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name = "data_atualizacao", nullable = false)
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ",dataCriacao=" + dataCriacao
+				+ ",dataAtualizacao=" + dataAtualizacao + "]";
+	}
 
 }
---
+++ src/main/java/br/com/dbarbosa/pontointeligente/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package br.com.dbarbosa.pontointeligente.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN, 
+	ROLE_USUARIO
+
+}
